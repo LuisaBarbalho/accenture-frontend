600e280ab7512062f6137ff90efea6869d3070fb
Christine Kim <christine.kim@rga.com>
2020-10-17 18:01:20 -0300

    add keyboard onclick event

--- accenture-frontend/src/component/Nokia.js
+++ accenture-frontend/src/component/Nokia.js
@@ -1,15 +1,12 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState } from 'react'
 import logo from '../smartphone.png';
 
 
 export default function Nokia() {
-
   const [message, setMessage] = useState('');
   const [convertedMsg, setConvertedMsg] = useState('');
 
-  const getInput = useCallback((input) => {
-    setMessage(`${message}${input}`);
-  }, []);
+  
 
   return (
     <div className="Wrapper">
@@ -21,24 +18,24 @@ export default function Nokia() {
       <button className="Enter">Enter</button>
       <div className="Container">
         <div className="Row">
-          <button onClick={() => getInput('1')}>1</button>
-          <button onClick={() => getInput('2')}>2 <span>[abc]</span></button>
-          <button onClick={() => getInput('3')}>3 <span>[def]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '1')}>1</button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '2')}>2 <span>[abc]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '3')}>3 <span>[def]</span></button>
         </div>
         <div className="Row">
-          <button onClick={() => getInput('4')}>4 <span>[ghi]</span></button>
-          <button onClick={() => getInput('5')}>5 <span>[jkl]</span></button>
-          <button onClick={() => getInput('6')}>6 <span>[mno]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '4')}>4 <span>[ghi]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '5')}>5 <span>[jkl]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '6')}>6 <span>[mno]</span></button>
         </div>
         <div className="Row">
-          <button onClick={() => getInput('7')}>7 <span>[pqrs</span>]</button>
-          <button onClick={() => getInput('8')}>8 <span>[tuv]</span></button>
-          <button onClick={() => getInput('9')}>9 <span>[wxyx</span>]</button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '7')}>7 <span>[pqrs</span>]</button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '8')}>8 <span>[tuv]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '9')}>9 <span>[wxyx</span>]</button>
         </div>
         <div className="Row">
-          <button onClick={() => getInput('*')}>* [+]</button>
-          <button onClick={() => getInput('0')}>0 <span>[ _ ]</span></button>
-          <button onClick={() => getInput('_')}>H <span>[#]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + '*')}>* [+]</button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + ' ')}>0 <span>[ _ ]</span></button>
+          <button onClick={() => setMessage(prevMsg => prevMsg + 'H')}>H <span>[#]</span></button>
         </div>
       </div>
     </div>
